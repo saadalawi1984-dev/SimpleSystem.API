99de1879b98fd6904270ed02f7d745a442f996df
Amr saad saad Ahmed alawi <saadalawi1984@gmail.com>
2026-08-07 22:07:21 +0300

    Refactor IUserRepository to extend IGenaricRepository
    Updated IUserRepository to inherit from IGenaricRepository and removed some methods.

--- SimpleSystem.DataAccess/Repositories/Interfaces/IUserRepository.cs
+++ SimpleSystem.DataAccess/Repositories/Interfaces/IUserRepository.cs
@@ -1,14 +1,9 @@
-﻿using SimpleSystem.DataAccess.Entities;
+using SimpleSystem.DataAccess.Entities;
 
 namespace SimpleSystem.DataAccess.Repositories.Interfaces
 {
-    public interface IUserRepository
+    public interface IUserRepository: IGenaricRepository
     {
-        List<User> GetAll();
-        User? GetById(int userId);
         User? GetByPersonId(int personId);
-        int Add(User user);
-        bool Update(User user);
-        bool Delete(int userId);
     }
 }
