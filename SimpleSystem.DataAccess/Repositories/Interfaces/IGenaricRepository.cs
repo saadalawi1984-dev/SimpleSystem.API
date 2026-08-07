d9fa56dffce4e75684480827f3b8b6cfeb42f89e
Amr saad saad Ahmed alawi <saadalawi1984@gmail.com>
2026-08-07 22:06:24 +0300

    Update IGenaricRepository.cs

--- SimpleSystem.DataAccess/Repositories/Interfaces/IGenaricRepository.cs
+++ SimpleSystem.DataAccess/Repositories/Interfaces/IGenaricRepository.cs
@@ -1,1 +1,19 @@
+using SimpleSystem.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace SimpleSystem.DataAccess.Repositories.Interfaces
+{
+    internal interface IGenaricRepository<T>
+    {
+        List<T> GetAll();
+        T? GetById(int entityId);
+        int Add(T entity);
+        bool Update(T entity);
+        bool Delete(int entityId);
+
+    }
+}
